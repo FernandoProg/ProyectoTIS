3740d579180b5cedc83f955fef49213c67cb7057
Fernando Cabezas <fcabezas@ing.ucsc.cl>
2021-11-25 23:12:59 -0300

    Cambios
    cambie esto

--- user/Consultas/create_contribucion.php
+++ user/Consultas/create_contribucion.php
@@ -1,11 +1,12 @@
 <?php
-    require("../../conexion.php");
+    require("conexion.php");
     $nombre_contribucion = $_POST["nombre_contribucion"];
     $correo_contribucion = $_POST["correo_contribucion"];
     $descripcion_contribucion = $_POST["descripcion_contribucion"];
     $departamento = $_POST["departamento"];
+    $fecha = date("Y,m,d");
 
-    $sql = "INSERT INTO usuario (nombre_usuario, contraseña, rol) VALUES ('$nombre_usuario','$contrasena','admin')";
+    $sql = "INSERT INTO contribucion (id_contribucion, nombre_contribucion, correo_contribucion, descripcion_contribucion, departamento, fecha) VALUES ('', '$nombre_contribucion', '$correo_contribucion', '$descripcion_contribucion', '$departamento', '$fecha')";
     $resultado = mysqli_query($conexion, $sql);
-    header('Location: ../index.php');
+    header('Location: ../ingreso_contribucion.php');
 ?>
--- user/ingreso_contribucion.php
+++ user/ingreso_contribucion.php
@@ -1,5 +1,5 @@
 <?php
-    require("../conexion.php");
+    require("Consultas/conexion.php");
 ?>    
 
 <!doctype html>
@@ -7,31 +7,48 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php require("../header.php")?>
+        <?php require("head.php")?>
         <title>Envío Contribución</title>
     </head>
     <body>
-        <?php require("../navbar.php")?>
+        <?php require("navbar.php")?>
         <div class="container">
-            <div class="row ">
+            <div class="row mt-5">
                 <div class="col-lg-12">
                     <h1 class="text-center">Ingresar Contribución</h1>
-                    <form action="create_contribucion.php" method="POST">
+                    <form action="Consultas/create_contribucion.php" method="POST">
                         <div class="mb-3">
                             <label class="form-label fw-bolder">Nombre de la contribución:</label>
-                            <input class="form-control" rows="3" maxlength="50" placeholder="Usuario" name="nombre_contribucion">
+                            <input class="form-control" rows="3" maxlength="50" placeholder="Creacion de areas recreativas" name="nombre_contribucion">
                         </div>
                         <div class="mb-3">
                             <label class="form-label fw-bolder">Correo Usuario:</label>
-                            <input class="form-control" rows="3" maxlength="50" placeholder="Usuario" name="correo_contribucion">
+                            <input class="form-control" rows="3" maxlength="50" placeholder="example@gmail.com" name="correo_contribucion">
                         </div>
                         <div class="mb-3">
                             <label class="form-label fw-bolder">Descripción:</label>
-                            <input class="form-control" rows="3" maxlength="1000" placeholder="Usuario" name="descripcion_contribucion">
+                            <textarea class="form-control" cols="30" rows="4" maxlength="1000" placeholder="Se solicita la posibilidad de mayor cantidad de areas recreativas" name="descripcion_contribucion"></textarea>
                         </div>
                         <div class="mb-3">
-                            <label class="form-label fw-bolder">Departamento:</label>
-                            <input class="form-control" rows="3" maxlength="50" placeholder="Usuario" name="departamento">
+                            <select class="form-select" name="departamento">
+                                <option hidden selected>Seleccione el departamento</option>
+                                <option value="Aseo y ornato">Aseo y ornato</option>
+                                <option value="Tránsito y transporte">Tránsito y transporte</option>
+                                <option value="Administración y Finanzas">Administración y Finanzas</option>
+                                <option value="Salud Municipal">Salud Municipal</option>
+                                <option value="Desarrollo comunitario">Desarrollo comunitario</option>
+                                <option value="Secretaría Municipal">Secretaría Municipal</option>
+                                <option value="Obras municipales">Obras municipales</option>
+                                <option value="Obras municipales">Secretaría Comunal de Planificación y Coordinación</option>
+                                <option value="Obras municipales">Medio ambiente</option>
+                                <option value="Obras municipales">Informática</option>
+                                <option value="Obras municipales">Control, gestión y desarrollo de personas</option>
+                                <option value="Obras municipales">Cultura</option>
+                                <option value="Obras municipales">Gestión de riesgos y emergencias</option>
+                                <option value="Obras municipales">Seguridad</option>
+                                <option value="Obras municipales">Relaciones públicas y prensa</option>
+                                <option value="Obras municipales">Jurídico</option>
+                            </select>
                         </div>
                         <input type="submit" class="btn btn-secondary" value="Guardar">
                     </form>

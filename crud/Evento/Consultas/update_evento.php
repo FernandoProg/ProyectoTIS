a01796d6d8be1ba59d07f6df18231f01deb26ea5
Bernardo-f <andres_bernardo15@hotmail.com>
2021-11-25 23:03:38 -0300

    error images

--- crud/Evento/Consultas/update_evento.php
+++ crud/Evento/Consultas/update_evento.php
@@ -5,16 +5,15 @@
     $idLugar =$_POST["idLugar"];
     $nombreEvento =$_POST["nombreEvento"];
     $fechaEvento =$_POST["fechaEvento"];
-    $imagenEvento = addslashes(file_get_contents($_FILES['imagenEvento']['tmp_name']));
     $descripcionEvento = $_POST["descripcionEvento"];
     
-    if($imagenEvento){
-        $sql = "UPDATE evento SET id_lugar='$idLugar',nombre_evento='$fechaEvento',fecha_evento='$fechaEvento',imagen_evento='$imagenEvento',descripcion_evento='$descripcionEvento' WHERE id_evento=$id";
+    if($_FILES['imagenEvento']['tmp_name']){
+        $imagenEvento = addslashes(file_get_contents($_FILES['imagenEvento']['tmp_name']));
+        $sql = "UPDATE evento SET id_lugar='$idLugar',nombre_evento='$nombreEvento',fecha_evento='$fechaEvento',imagen_evento='$imagenEvento',descripcion_evento='$descripcionEvento' WHERE id_evento=$id";
+        $consulta = mysqli_query($conexion,$sql);
     }else{
-        $sql = "UPDATE evento SET id_lugar='$idLugar',nombre_evento='$fechaEvento',fecha_evento='$fechaEvento',descripcion_evento='$descripcionEvento' WHERE id_evento=$id";
+        $sql = "UPDATE evento SET id_lugar='$idLugar',nombre_evento='$nombreEvento',fecha_evento='$fechaEvento',descripcion_evento='$descripcionEvento' WHERE id_evento=$id";
+        $consulta = mysqli_query($conexion,$sql);
     }
-    
-    $consulta = mysqli_query($conexion,$sql);
-
     header('location: ../');        
 ?>

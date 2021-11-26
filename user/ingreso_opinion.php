120cc1c1c8a140f90c80dccc0d410412b5d741d4
Bernardo-f <andres_bernardo15@hotmail.com>
2021-11-25 22:31:42 -0300

    ingreso_opinion

--- user/ingreso_opinion.php
+++ user/ingreso_opinion.php
@@ -13,23 +13,35 @@
     <div class="container">
 
     </div>
-    <div class="container">
-        <form action="">
+    <div class="container mt-4">
+            <div class="row fs-2 fw-bolder text-center">
+                <span>INGRESO OPINION</span>
+            </div>
             <div class="row mt-4">
                 <div class="col">
                     <label class="form-label fw-bolder">Nombre:</label>
-                    <input class="form-control" type="correo" name="nombre">
+                    <input class="form-control" type="correo" name="nombre_opinion" required>
                 </div>
                 <div class="col">
                     <label class="form-label fw-bolder">Correo:</label>
-                    <input class="form-control" type="text" name="correo">
+                    <input class="form-control" type="text" name="correo_opinion" required>
                 </div>
             </div>
-            <div class="row">
-                <div class="col mt-4">
+            <div class="row mt-4">
+                <div class="col">
                     <label class="form-laber fw-bolder">Descripción:</label>
-                    <textarea name="descripcion" class="form-control" cols="30" rows="10"></textarea>
+                    <textarea name="descripcion_opinion" class="form-control" cols="30" rows="10" required></textarea>
                 </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col">
+                    <label class="form-label fw-bolder">Imagenes:</label>
+                    <input accept="image/png, .jpeg, .jpg, .svg" class="form-control" type="file" multiple name="imagenes_opinion[]" id="">
+                    <label class="">Se puede ingresar un máximo de 5 imagenes.</label>
+                </div>
+            </div>
+            <div class="row w-25 mx-auto mt-4">
+                <input type="submit" class="btn btn-secondary" value="ingresar Opinion">
             </div>
         </form>
     </div>

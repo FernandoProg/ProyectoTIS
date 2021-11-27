dad1b737a44a1bffc744650705ba177cff0be9be
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2021-11-27 14:44:53 -0300

    Update view_noticias.php

---
+++ public/view_noticias.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Municipalidad</title>
+</head>
+<body>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>

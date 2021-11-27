f121652d62b2d7a65207d66ce91c9b1d062baddf
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2021-11-27 11:51:37 -0300

    Update editar_noticia.php

--- crud/Noticia/editar_noticia.php
+++ crud/Noticia/editar_noticia.php
@@ -5,7 +5,7 @@
     $resultado = mysqli_query($conexion, $consulta);
 
     while($row = mysqli_fetch_assoc($resultado)){
-        $get_nombre_admin = $row["nombre_admin"];
+        $get_nombre_usuario = $row["nombre_usuario"];
         $get_titulo_noticia = $row["titulo_noticia"];
         $get_fecha_noticia = $row["fecha_noticia"];
         $get_lead_noticia = $row["lead_noticia"];
@@ -37,13 +37,13 @@
               </div>
               <!-- UPDATE -->
               <form action="Consultas/update_noticia.php" enctype="multipart/form-data" method="POST">
-                <input type="hidden" class="form-control" name="insert_nombre_admin" value="<?php echo "$get_nombre_admin" ?>">
+                <input type="hidden" class="form-control" name="insert_nombre_usuario" value="<?php echo "$get_nombre_usuario" ?>">
                 <div class="mb-3"> 
                   <label class="form-label fw-bolder" >Fecha:</label>
                   <input type="date" class="form-control" name="insert_fecha_noticia" value="<?php echo "$get_fecha_noticia" ?>"> 
                 </div>
                 <div class="mb-3">
-                  <label class="form-label fw-bolder">Titulo:</label>
+                  <label class="form-label fw-bolder">Título:</label>
                   <input type="text" class="form-control" name="insert_titulo_noticia" value="<?php echo "$get_titulo_noticia" ?>">
                 </div>
                 <div class="mb-3">
@@ -59,7 +59,7 @@
                   <textarea name="insert_cuerpo_noticia" class="form-control" cols="30" rows="10" maxlength="10000" ><?php echo "$get_cuerpo_noticia" ?></textarea>
                 </div>
                 <div class="mb-3">
-                  <label class="form-label fw-bolder" >Categoria:</label>
+                  <label class="form-label fw-bolder" >Categoría:</label>
                   <!--<input type="text" class="form-control" name="insert_categoria_noticia">-->
                   <select class="form-select" name="insert_categoria_noticia" >
                         <option hidden selected value= "<?php echo $get_categoria_noticia ?>"><?php echo $get_categoria_noticia ?></option>
@@ -84,7 +84,7 @@
                     <button class="btn btn-secondary" type="submit">Editar</button>
                   </div>
                   <div class="col-lg-6 " >
-                    <a href="../Noticia"  class="btn btn-primary" >Volver</a>
+                    <a href="index.php"  class="btn btn-primary" >Volver</a>
                   </div>
                 </div>
               </form>

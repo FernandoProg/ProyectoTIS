e1814c0c62f96e0963f2e66ecf184db2dfdb3c55
Fernando Cabezas <fcabezas@ing.ucsc.cl>
2021-11-27 23:42:46 -0300

    Lugar
    Mejora de lugar

--- crud/Lugar/Consultas/create_lugar.php
+++ crud/Lugar/Consultas/create_lugar.php
@@ -4,11 +4,14 @@
     $nombreLugar = $_POST["nombreLugar"];
     $latitudLugar = $_POST["latitudLugar"];
     $longitudLugar = $_POST["longitudLugar"];
-    $categoriaLugar = $_POST["categoriaLugar"];
-    $nombreAdmin = $_POST["nombreAdmin"];
-
-    $query = "INSERT INTO lugar VALUES ('','$nombreLugar','$latitudLugar','$longitudLugar','$categoriaLugar','$nombreAdmin')";
-    $insert = mysqli_query($conexion,$query);
-    
-    header('location: ../');
+    if(intval($latitudLugar) == 0 && intval($longitudLugar) == 0){
+        echo '<script>alert("No haz seleccionado un lugar");</script>';
+        echo '<script>window.location.href="../index.php"</script>';
+    }else{
+        $categoriaLugar = $_POST["categoriaLugar"];
+        session_start();
+        $query = "INSERT INTO lugar VALUES ('','$nombreLugar','$latitudLugar','$longitudLugar','$categoriaLugar','".$_SESSION['nombre_usuario']."')";
+        $insert = mysqli_query($conexion,$query);
+        header('location: ../');
+    }
 ?>

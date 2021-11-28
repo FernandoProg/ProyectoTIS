4d13226b2f33f9efdb0dff9d62597d61bdb9eb8e
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2021-11-28 01:17:01 -0300

    Create conexion.php

---
+++ public/Noticia/conexion.php
@@ -0,0 +1,8 @@
+<?php
+    $conexion = mysqli_connect("localhost", "root","","proyecto_municipalidad");
+    if($conexion->connect_error){
+        die("Conexión fallida ". $conn->connect_error);
+    }
+
+
+?>

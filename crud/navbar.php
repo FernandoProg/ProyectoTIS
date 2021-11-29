07225deb67c8b0eafb2b8131bb91fd877e0e6124
Fernando Cabezas <fcabezas@ing.ucsc.cl>
2021-11-29 00:31:11 -0300

    Update navbar.php

--- crud/navbar.php
+++ crud/navbar.php
@@ -30,51 +30,13 @@ if($_SESSION['rol'] == "admin"){ ?>
                 </li>
             </ul>
             <ul class="nav justify-content-end">
-            <li class="nav-item">
-                    <a class="nav-link text-white"  href="../../sesion_usuarios/logout.php">Cerrar Sesión</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white"  href="../..">Acceso Funcionarios</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-<?php } 
-    else{ ?>
-      <nav class="navbar bg-primary mb-4">
-        <div class="container-fluid">   
-            <a class="navbar-brand" href="#">
-                <img src="../img/logo.png" style="width: 40%;">
-            </a>
-            <ul class="nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-current="page" href="../Evento">Eventos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../Noticia">Noticias</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../Emprendedor">Emprendedores</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../Lugar">Mapa Comuna</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../Opinion">Opiniones</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../Contribucion">Contribuciones</a>
-                </li>
-            </ul>
-            <ul class="nav justify-content-end">
-            <li class="nav-item">
-                    <a class="nav-link text-white"  href="../../sesion_usuarios/logout.php">Cerrar Sesión</a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link text-white"  href="../../sesion_usuarios/logout.php">Cerrar Sesión - <?php echo $_SESSION['nombre_usuario']; ?></a>
                 </li>
             </ul>
         </div>
     </nav>
-  
-<?php } ?>
+<?php } 
+    else{ 
+        header("Location: ../index.php");
+    } ?>

58feffe2f560dbd224c9b4d4b7b27a4f5739ccac
Fernando Cabezas <fcabezas@ing.ucsc.cl>
2021-11-28 23:26:25 -0300

    Update create_opinion.php

--- user/Consultas/create_opinion.php
+++ user/Consultas/create_opinion.php
@@ -6,12 +6,14 @@
     $length = count($_FILES["imagenes_opinion"]['name']);
     $fecha = date("Y,m,d");
     $query= "INSERT INTO opinion VALUES ('','$nombre_opinion','".$_SESSION['nombre_usuario']."','$descripcion_opinion','$fecha')";
+    echo $length;
     if($length>5){
         echo'<script>
         alert("No se pueden ingresar mas de 5 imagenes");
         window.location.href = "../ingreso_opinion.php";
         </script>';
-    }else{
+    }else if($length > 0){
+        
         $ingreso = mysqli_query($conexion,$query) or die(mysqli_error($conexion));
         $id= mysqli_insert_id($conexion);   
         for($i=0 ; $i<$length ;$i++){
@@ -20,6 +22,9 @@
             $query_image = "INSERT INTO imagen_opinion VALUES('','$id','$imagen_opinion','$tipo_imagen')";
             $consulta = mysqli_query($conexion,$query_image);
         }
-        header('location: ../ingreso_opinion.php');
+        
+        //header('location: ../ingreso_opinion.php');
+    }else{
+        $ingreso = mysqli_query($conexion,$query) or die(mysqli_error($conexion));
     }
 ?>

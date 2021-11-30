4c62ed7e62b5cfba4627f31c6c65a76b1b71ffb4
Fernando Cabezas <fcabezas@ing.ucsc.cl>
2021-11-29 21:08:07 -0300

    NavbarResponsives

--- navbar_user.php
+++ navbar_user.php
@@ -1,50 +1,14 @@
 <style>
-      a:hover {
-       color:rgb(194, 190, 190) !important;
-      }
+    a:hover {
+    color:rgb(194, 190, 190) !important;
+    }
 </style>
 <?php
-        if(!isset($_SESSION)){
-            session_start();
-        }
-        if(isset($_SESSION['nombre_usuario'])){
-            if($_SESSION['rol'] == "usuario"){ // Navbar para usuarios logeados
-                ?>
-                <nav class="navbar  bg-primary mb-4">
-                    <div class="container-fluid">   
-                        <a class="navbar-brand" href="index.php">
-                            <img src="/proyecto-municipalidad/crud/img/logo.png" style="width: 40%;">
-                        </a>
-                        <ul class="nav mx-auto">
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="public/view_noticias.php">Noticias</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="user/ingreso_opinion.php">Opiniones</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="user/ingreso_contribucion.php">Contribuciones</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="public/view_eventos.php">Eventos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="public/view_emprendedores.php">Emprendedores</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="public/view_mapa.php">Mapa comuna</a>
-                            </li>
-                        </ul>
-                        <ul class="nav justify-content-end">
-                        <li class="nav-item">
-                                <a class="nav-link text-white"  href="sesion_usuarios/logout.php">Cerrar Sesión - <?php echo $_SESSION['nombre_usuario']; ?></a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                <?php
-            }
-        }else{ // Navbar para usuarios no logeados
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    if(isset($_SESSION['nombre_usuario'])){
+        if($_SESSION['rol'] == "usuario"){ // Navbar para usuarios logeados
             ?>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container-fluid  ">
@@ -54,8 +18,48 @@
                     <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                         <a class="navbar-brand" href="index.php"><img src="crud/img/logo.png" style="width: 40%;"></a>
-
                         </ul>
+                        <ul class="navbar-nav ">
+                            <li class="nav-item">
+                                <a class="nav-link text-white  " aria-current="" href="public/view_noticias.php">Noticias</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="../user/ingreso_opinion.php">Opiniones</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="../user/ingreso_contribucion.php">Contribuciones</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white " href="public/view_eventos.php">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white " href="public/view_emprendedores.php">Emprendedores</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white " href="public/view_mapa.php">Mapa Comuna</a>
+                            </li>
+                        </ul>
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link text-white"  href="sesion_usuarios/logout.php">Cerrar Sesión - <?php echo $_SESSION['nombre_usuario']; ?></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <?php
+        }
+    }else{ // Navbar para usuarios no logeados
+        ?>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid  ">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <a class="navbar-brand" href="index.php"><img src="crud/img/logo.png" style="width: 40%;"></a>
+                    </ul>
                     <ul class="navbar-nav ">
                         <li class="nav-item">
                             <a class="nav-link text-white  " aria-current="" href="public/view_noticias.php">Noticias</a>
@@ -69,19 +73,18 @@
                         <li class="nav-item">
                             <a class="nav-link text-white " href="public/view_mapa.php">Mapa Comuna</a>
                         </li>
-                
                     </ul>
                     <ul class="navbar-nav">
-                         <li class="nav-item">
+                        <li class="nav-item">
                             <a class="nav-link text-white " href="acceso_usuarios.php">Inicio Usuarios</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white " href="acceso_funcionarios.php">Acceso Funcionarios</a>
                         </li>
                     </ul>
-                    </div>
                 </div>
-            </nav>
-            <?php
-        }
-    ?>
+            </div>
+        </nav>
+        <?php
+    }
+?>
--- public/navbar_user.php
+++ public/navbar_user.php
@@ -1,75 +1,85 @@
 <?php
-        if(!isset($_SESSION)){
-            session_start();
-        }
-        if(isset($_SESSION['nombre_usuario'])){
-            if($_SESSION['rol'] == "usuario"){ // Navbar para usuarios logeados
-                ?>
-                    <nav class="navbar bg-primary mb-4">
-                        <div class="container-fluid">   
-                            <a class="navbar-brand" href="../index.php">
-                                <img src="../crud/img/logo.png" style="width: 40%;">
-                            </a>
-                            <ul class="nav mx-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="view_noticias.php">Noticias</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="../user/ingreso_opinion.php">Opiniones</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="../user/ingreso_contribucion.php">Contribuciones</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="view_eventos.php">Eventos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="view_emprendedores.php">Emprendedores</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="view_mapa.php">Mapa comuna</a>
-                                </li>
-                            </ul>
-                            <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                    <a class="nav-link text-white"  href="../sesion_usuarios/logout.php">Cerrar Sesión - <?php echo $_SESSION['nombre_usuario']; ?></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                <?php
-            }
-        }else{ // Navbar para usuarios no logeados
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    if(isset($_SESSION['nombre_usuario'])){
+        if($_SESSION['rol'] == "usuario"){ // Navbar para usuarios logeados
             ?>
-                <nav class="navbar bg-primary mb-4">
-                    <div class="container-fluid">   
-                        <a class="navbar-brand" href="../index.php">
-                            <img src="../crud/img/logo.png" style="width: 40%;">
-                        </a>
-                        <ul class="nav mx-auto">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid  ">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <a class="navbar-brand" href="../index.php"><img src="../crud/img/logo.png" style="width: 40%;"></a>
+                        </ul>
+                        <ul class="navbar-nav ">
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="view_noticias.php">Noticias</a>
+                                <a class="nav-link text-white " aria-current="" href="view_noticias.php">Noticias</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="view_eventos.php">Eventos</a>
+                                <a class="nav-link text-white" href="../user/ingreso_opinion.php">Opiniones</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="view_emprendedores.php">Emprendedores</a>
+                                <a class="nav-link text-white" href="../user/ingreso_contribucion.php">Contribuciones</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="view_mapa.php">Mapa comuna</a>
+                                <a class="nav-link text-white " href="view_eventos.php">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white " href="view_emprendedores.php">Emprendedores</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white " href="view_mapa.php">Mapa Comuna</a>
                             </li>
                         </ul>
                         <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link text-white"  href="../acceso_usuarios.php">Inicio Usuarios</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white"  href="../acceso_funcionarios.php">Acceso Funcionarios</a>
-                            </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white"  href="../sesion_usuarios/logout.php">Cerrar Sesión - <?php echo $_SESSION['nombre_usuario']; ?></a>
+                        </li>
                         </ul>
                     </div>
-                </nav>
+                </div>
+            </nav>
             <?php
         }
-    ?>
+    }else{ // Navbar para usuarios no logeados
+        ?>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid  ">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <a class="navbar-brand" href="index.php"><img src="../crud/img/logo.png" style="width: 40%;"></a>
+                    </ul>
+                    <ul class="navbar-nav ">
+                        <li class="nav-item">
+                            <a class="nav-link text-white " aria-current="" href="view_noticias.php">Noticias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " href="view_eventos.php">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " href="view_emprendedores.php">Emprendedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " href="view_mapa.php">Mapa Comuna</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-white " href="../acceso_usuarios.php">Inicio Usuarios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " href="../acceso_funcionarios.php">Acceso Funcionarios</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php
+    }
+?>

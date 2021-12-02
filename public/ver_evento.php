01caddf82177bd03fa00c7ea0523ff324b4bfe1a
Bernardo-f <andres_bernardo15@hotmail.com>
2021-12-01 21:45:34 -0300

    asist-1

--- public/ver_evento.php
+++ public/ver_evento.php
@@ -6,6 +6,9 @@
     $resultado = mysqli_query($conexion, $consulta);
     $visita = "UPDATE evento SET visitas_evento = visitas_evento +1 WHERE id_evento = $id";
     $sumarVisita = mysqli_query($conexion, $visita);
+    $sqlAsist = "SELECT COUNT(nombre_usuario) asist FROM asiste WHERE id_evento = $id";
+    $asist = mysqli_query($conexion,$sqlAsist);
+    $numAsist = mysqli_fetch_assoc($asist);
     while($row = mysqli_fetch_assoc($resultado)){
         $nombreEvento = $row["nombre_evento"];
         $fechaEvento = $row["fecha_evento"];
@@ -15,56 +18,84 @@
     }
 ?>
 
-<!doctype html>
-  <html lang="es">
-    <head>
-      <meta charset="utf-8">
-      <?php require("../user/head.php")?>
-      <title>Ver más - Evento</title>
-    </head>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <?php require("../user/head.php")?>
+    <title>Ver más - Evento</title>
+  </head>
 
-    <body>   
-        <?php require("navbar_user.php");?>  
+  <body>
+    <?php require("navbar_user.php");?>
 
-        <div class="container mb-5">
-            <div class="row col-lg-12">
-                <!-- TITULO -->
-                <div class="row col-lg-12">
-                    <span class="fs-1 fw-bolder text-center"><?php echo $nombreEvento ?></span>
-                </div>
-                <!-- Fecha -->
-                <div class="row col-lg-12">
-                    <span class="fs-5  text-left ">Fecha de realización: <?php echo $fechaEvento ?></span>
-                </div>
-                
-                <div class="row">
-                    <!-- IMAGEN -->
-                    <div class="col-lg-5 col-sm-12  "> 
-                        <img class="mt-5" style="width:100%;" src="data:image/jpeg;base64,<?php echo base64_encode($imagenEvento)?>" >
-                    </div>
-                    <div class="col-lg-7 col-sm-12">
-                        <!-- Lead -->
-                        <div class="col-lg-12 pt-3" >
-                            <span class="fs-5 text-justify d-flex " style="text-align: justify;"><?php echo $nombreLugar ?></span>
-                        </div>
-                        <!-- CUERPO -->
-                        <div class="col-lg-12 pt-3  ">
-                            <span class="fs-5 text-justify d-flex " style="text-align: justify;"class="fs-5 text-justify d-flex " style="text-align: justify;"><?php echo $descripcionEvento ?></span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <a href="view_eventos.php"  class="btn btn-primary" >Volver</a>
-                    </div>
-                </div>
-                
-            </div>
+    <div class="container mb-5">
+      <div class="row col-lg-12">
+        <!-- TITULO -->
+        <div class="row col-lg-12">
+          <span class="fs-1 fw-bolder text-center"
+            ><?php echo $nombreEvento ?></span
+          >
         </div>
-        <?php require("../footer.php"); ?>
-    </body>
+        <!-- Fecha -->
+        <div class="row col-lg-12">
+          <span class="fs-5 text-left"
+            >Fecha de realización:
+            <?php echo $fechaEvento ?></span
+          >
+        </div>
+
+        <div class="row">
+          <!-- IMAGEN -->
+          <div class="col-lg-5 col-sm-12">
+            <img
+              class="mt-5"
+              style="width: 100%"
+              src="data:image/jpeg;base64,<?php echo base64_encode($imagenEvento)?>"
+            />
+          </div>
+          <div class="col-lg-7 col-sm-12">
+            <!-- Lead -->
+            <div class="col-lg-12 pt-3">
+              <span class="fs-5 text-justify d-flex" style="text-align: justify"
+                ><?php echo $nombreLugar ?></span
+              >
+            </div>
+            <!-- CUERPO -->
+            <div class="col-lg-12 pt-3">
+              <span
+                class="fs-5 text-justify d-flex"
+                style="text-align: justify"
+                class="fs-5 text-justify d-flex"
+                style="text-align: justify"
+                ><?php echo $descripcionEvento ?></span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-lg-6 text-lg-end text-sm-center col-md-12 p-2">
+            <div class="">
+              <span class="fs-3 align-bottom material-icons"> people </span>
+              <span class="fs-5  fw-bolder">
+                Asistencia:
+                <?php echo$numAsist["asist"]?>
+              </span>
+            </div>
+          </div>
+          <div class="col-lg-6 text-lg-start text-sm-center col-md-12">
+            <span class="">
+              <a class="btn btn-secondary" href=""> Marcar Asistencia</a>
+            </span>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-lg-12 text-center">
+            <a href="view_eventos.php" class="btn btn-primary">Volver</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php require("../footer.php"); ?>
+  </body>
 </html>
-
-
-
